8b314e06b52b811171d26cf67e64ac16105596f1
Jesper Dangaard Brouer <brouer@redhat.com>
2018-06-12 14:27:26 +0200

    samples/bpf: extend xdp_rxq_info to read packet payload
    There is a cost associated with reading the packet data payload
    that this test ignored.  Add ain option --read to allow enabling
    reading part of the payload.
    
    With no_touch of data:
    
    Running XDP on dev:mlx5p1 (ifindex:8) action:XDP_DROP options:no_touch
    XDP stats       CPU     pps         issue-pps
    XDP-RX CPU      0       14,465,157  0
    XDP-RX CPU      1       14,464,728  0
    XDP-RX CPU      2       14,465,283  0
    XDP-RX CPU      3       14,465,282  0
    XDP-RX CPU      4       14,464,159  0
    XDP-RX CPU      5       14,465,379  0
    XDP-RX CPU      total   86,789,992
    
    When not touching data, we observe that the CPUs have idle cycles.
    When reading data the CPUs are 100% busy in softirq.
    
    With reading data;
    
    Running XDP on dev:mlx5p1 (ifindex:8) action:XDP_DROP options:read
    XDP stats       CPU     pps         issue-pps
    XDP-RX CPU      0       9,620,639   0
    XDP-RX CPU      1       9,489,843   0
    XDP-RX CPU      2       9,407,854   0
    XDP-RX CPU      3       9,422,289   0
    XDP-RX CPU      4       9,321,959   0
    XDP-RX CPU      5       9,395,242   0
    XDP-RX CPU      total   56,657,828
    
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

--- samples/bpf/xdp_rxq_info_kern.c
+++ samples/bpf/xdp_rxq_info_kern.c
@@ -4,6 +4,8 @@
  *  Example howto extract XDP RX-queue info
  */
 #include <uapi/linux/bpf.h>
+#include <uapi/linux/if_ether.h>
+#include <uapi/linux/in.h>
 #include "bpf_helpers.h"
 
 /* Config setup from with userspace
@@ -14,6 +16,11 @@
 struct config {
 	__u32 action;
 	int ifindex;
+	__u32 options;
+};
+enum cfg_options_flags {
+	NO_TOUCH = 0x0U,
+	READ_MEM = 0x1U,
 };
 struct bpf_map_def SEC("maps") config_map = {
 	.type		= BPF_MAP_TYPE_ARRAY,
@@ -75,6 +82,18 @@ int  xdp_prognum0(struct xdp_md *ctx)
 		/* count this error case */
 		rec->issue++;
 		return XDP_ABORTED;
+	}
+
+	/* Default: Don't touch packet data, only count packets */
+	if (unlikely(config->options & READ_MEM)) {
+		struct ethhdr *eth = data;
+
+		if (eth + 1 > data_end)
+			return XDP_ABORTED;
+
+		/* Avoid compiler removing this: Drop non 802.3 Ethertypes */
+		if (ntohs(eth->h_proto) < ETH_P_802_3_MIN)
+			return XDP_ABORTED;
 	}
 
 	/* Update stats per rx_queue_index. Handle if rx_queue_index
